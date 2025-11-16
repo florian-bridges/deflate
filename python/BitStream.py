375fd1a7fe555f58e3c7ff52539cdbbb715fb124
florian <florianbridges97@gmail.com>
2025-11-16 11:25:15 +0100

    refactor: move bit functions to BitStream and remove bytearrays

--- python/BitStream.py
+++ python/BitStream.py
@@ -8,6 +8,13 @@ class BitStream:
         self.bit_idx = 0
         pass
 
+    def reverse_bits(self, bits, num_bits):
+        result = 0
+        for _ in range(num_bits):
+            result = (result << 1) | (bits & 1)
+            bits >>= 1
+        return result
+
     def append_bit(self, bit):
         if self.bit_idx == 8:
             self.bit_stream.append(self.new_byte)
@@ -24,6 +31,18 @@ class BitStream:
             )
 
             bits >>= 1
+
+    def append_reverse(self, bits, num_bits=1):
+        bits = self.reverse_bits(bits, num_bits)
+        self.append(bits, num_bits)
+
+    def append_numerical(self, value, num_bytes=1):
+
+        if value > 2**(num_bytes * 8):
+            raise ValueError(f"length of bitstream has to be less than {2**(num_bytes * 8)} bit is {value}")
+
+        for i in range(num_bytes):
+            self.append((value >> (8 * i)) & 0xFF, 8)
 
     def get(self):
         if self.bit_idx == 0:
