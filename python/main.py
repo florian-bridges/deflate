619ee51f56b2c88b69e72c241b4018b06da69463
florian <florian.bridges@tum.de>
2025-11-16 11:25:15 +0100

    refactor: move bit functions to BitStream and remove bytearrays

--- python/main.py
+++ python/main.py
@@ -4,8 +4,11 @@ import zlib
 from python.BitStream import BitStream
 
 
-IN_FILE_PATH = "data/lorem_ipsum.txt"
-OUT_FILE_PATH = "test_out/lorem_ipsum.txt.gz"
+IN_FILE_PATH = "data/hello_world.txt"
+OUT_FILE_PATH = "test_out/hello_world.txt.gz"
+
+#IN_FILE_PATH = "data/lorem_ipsum.txt"
+#OUT_FILE_PATH = "test_out/lorem_ipsum.txt.gz"
 
 
 # gzip file header variables
@@ -58,227 +61,173 @@ for code, num_bits, lower_bound, upper_bound in DISTANCE_CODE_RANGES:
 
 MAX_REF_DISTANCE = 32768
 
-def to_gzip_numerical(value, num_bytes):
-
-    if value > 2**(num_bytes * 8):
-        raise ValueError(f"length of bitstream has to be less than {2**(num_bytes * 8)} bit is {value}")
-
-    out_bytes = bytearray()
-
-    for i in range(num_bytes):
-        out_bytes.append(
-            (value >> (8 * i)) & 0xFF
-        )
-
-    return out_bytes
-
 
 def build_header():
     
-    out_stream = bytearray()
+    out_stream = BitStream()
 
-    out_stream.append(GZIP_HEADER_ID1)
-    out_stream.append(GZIP_HEADER_ID2)
-    out_stream.append(GZIP_HEADER_CM_DEFLATE)
+    out_stream.append_numerical(GZIP_HEADER_ID1)
+    out_stream.append_numerical(GZIP_HEADER_ID2)
+    out_stream.append_numerical(GZIP_HEADER_CM_DEFLATE)
+    out_stream.append(GZIP_HEADER_FLG_FTEXT)
+    out_stream.append(GZIP_HEADER_FLG_FHCRC)
+    out_stream.append(GZIP_HEADER_FLG_FEXTRA)
+    out_stream.append(GZIP_HEADER_FLG_FNAME)
+    out_stream.append(GZIP_HEADER_FLG_FCOMMENT)
+    out_stream.append(0, 3)
+    out_stream.append_numerical(GZIP_HEADER_MTIME, 4)
+    out_stream.append_numerical(GZIP_HEADER_XFL)
+    out_stream.append_numerical(GZIP_HEADER_OS)
 
-    gzip_header_flag = 0x00
-    gzip_header_flag |= GZIP_HEADER_FLG_FTEXT
-    gzip_header_flag |= GZIP_HEADER_FLG_FHCRC << 1
-    gzip_header_flag |= GZIP_HEADER_FLG_FEXTRA << 2
-    gzip_header_flag |= GZIP_HEADER_FLG_FNAME << 3
-    gzip_header_flag |= GZIP_HEADER_FLG_FCOMMENT << 4
-    out_stream.append(gzip_header_flag)
-
-    out_stream += to_gzip_numerical(GZIP_HEADER_MTIME, 4)
-
-    out_stream.append(GZIP_HEADER_XFL)
-    out_stream.append(GZIP_HEADER_OS)
-
-    return out_stream
+    return out_stream.get()
 
 def build_footer(in_stream):
 
-    out_stream = bytearray()
+    out_stream = BitStream()
 
-    crc = zlib.crc32(in_stream)
-    out_stream += to_gzip_numerical(crc, 4)
+    out_stream.append_numerical(zlib.crc32(in_stream), 4)
+    out_stream.append_numerical(len(in_stream), 4)
 
-
-    len_bitstream = len(in_stream)
-    out_stream += to_gzip_numerical(len_bitstream, 4)
-
-    return out_stream
+    return out_stream.get()
 
 
 
 def block_type_0(in_stream, is_last=1):
 
     block_type = 0
-    out_stream = bytearray()
+    out_stream = BitStream()
 
-    block_header = 0x00
-    block_header |= is_last << 0
-    block_header |= block_type  << 1
-    out_stream.append(block_header)
+    out_stream.append(is_last)
+    out_stream.append(block_type, 2)
+    out_stream.append(0, 5)
 
     len_bitstream = len(in_stream)
-    out_stream += to_gzip_numerical(len_bitstream, 2)
-    out_stream += to_gzip_numerical(~len_bitstream, 2)
+    out_stream.append_numerical(len_bitstream, 2)
+    out_stream.append_numerical(~len_bitstream, 2)
 
-    out_stream += in_stream
+    return out_stream.get() + in_stream
 
-    return out_stream
-    
-
-def get_huffman_codes():
-    """
-    Lit Value    Bits        Codes
-    ---------    ----        -----
-      0 - 143     8          00110000 through
-                            10111111
-    144 - 255     9          110010000 through
-                            111111111
-    256 - 279     7          0000000 through
-                            0010111
-    280 - 287     8          11000000 through
-                            11000111
-    """
-
-    tree_lengths = {}
-    for i in range(144):
-        tree_lengths[i] = 8
-    for i in range(144, 256):
-        tree_lengths[i] = 9
-    for i in range(256, 280):
-        tree_lengths[i] = 7
-    for i in range(280, 288):
-        tree_lengths[i] = 8
-    
+def get_huffman_codes(length_list):
 
     bl_count = {}
-    for i in range(7):
-        bl_count[i] = 0
+    tree_lengths = {}
 
-    bl_count[7] = 279 - 256 + 1
-    bl_count[8] = 143 -   0 + 1
-    bl_count[8] = 287 - 280 + 1
-    bl_count[9] = 255 - 144 + 1
+    max_num_bits = 0
+
+    for from_label, to_label, num_bits in length_list:
+
+        for label in range(from_label, to_label + 1):
+            tree_lengths[label] = num_bits
+
+        if num_bits not in bl_count.keys():
+            bl_count[num_bits] = to_label - from_label + 1
+        else:
+            bl_count[num_bits] += to_label - from_label + 1
+
+        max_num_bits = max(max_num_bits, num_bits)
 
     code = 0
-
     next_code = {}
-    for bits in range(1, len(bl_count.keys())):
-        code += bl_count[bits-1]
+    for bits in range(1, max_num_bits + 1):
+        code += bl_count[bits-1] if bits-1 in bl_count.keys() else 0
         code <<= 1
         next_code[bits] = code
 
     tree_codes = {}
-    for n in range(288):
+    for n in range(len(tree_lengths)):
         tree_len = tree_lengths[n]
         if tree_len > 0:
             tree_codes[n] = next_code[tree_len]
             next_code[tree_len] += 1
         
     return tree_codes, tree_lengths
+
+
+def get_default_codes():
+    # from_label, to_label, num_bits
+    length_list = [
+        (0, 143, 8),
+        (144, 255, 9),
+        (256, 279, 7),
+        (280, 287, 8),
+    ]
+
+    return get_huffman_codes(length_list)
 
 def get_distance_codes():
+    # from_label, to_label, num_bits
+    length_list = [
+        (0, 32768, 5),
+    ]
 
-    tree_lengths = {}
-    for i in range(32769):
-        tree_lengths[i] = 5
+    return get_huffman_codes(length_list)
+
+
+def find_reference(byte_stream, byte_idx):
+    ref_dist = 3
+    ref_length = 0
+    while ref_dist >= 0:
+
+        if ref_dist > byte_idx or ref_dist >= MAX_REF_DISTANCE:
+            break
+
+        ref_length = 0
+
+        while byte_stream[byte_idx + ref_length] == byte_stream[byte_idx - ref_dist + ref_length]:
+
+            if ref_length >= len(byte_stream) or ref_length > MAX_REF_LENGTH:
+                break
+
+            ref_length += 1
+
+        if ref_length >= 3:
+            break
+
+        ref_dist += 1
+    return ref_length, ref_dist
+
     
-
-    bl_count = {}
-    for i in range(5):
-        bl_count[i] = 0
-
-    bl_count[5] = 32769
-    
-    code = 0
-
-    next_code = {}
-    for bits in range(1, len(bl_count.keys())):
-        code += bl_count[bits-1]
-        code <<= 1
-        next_code[bits] = code
-
-    tree_codes = {}
-    for n in range(288):
-        tree_len = tree_lengths[n]
-        if tree_len > 0:
-            tree_codes[n] = next_code[tree_len]
-            next_code[tree_len] += 1
-        
-    return tree_codes, tree_lengths
-
-def reverse_bits(value, width):
-    result = 0
-    for _ in range(width):
-        result = (result << 1) | (value & 1)
-        value >>= 1
-    return result
 
 def block_type_1(in_stream, is_last=1):
 
-    tree_codes, tree_lengths = get_huffman_codes()
+    tree_codes, tree_lengths = get_default_codes()
     distance_codes, distance_lengths = get_distance_codes()
     
     block_type = 1
     out_stream = BitStream()
 
-    out_stream.append(
-        is_last
-    )
-    out_stream.append(
-        block_type, 2
-    )
+    out_stream.append(is_last)
+    out_stream.append(block_type, 2)
 
     # add block termination token
     byte_stream = [int(byte) for byte in in_stream] + [256]
 
     byte_idx = 0
     while byte_idx < len(byte_stream):
-
         byte = byte_stream[byte_idx]
 
-        ref_dist = 3
-        ref_length = 0
-        while ref_dist >= 0:
-
-            if ref_dist > byte_idx or ref_dist >= MAX_REF_DISTANCE:
-                break
-
-            ref_length = 0
-
-            while byte_stream[byte_idx + ref_length] == byte_stream[byte_idx - ref_dist + ref_length]:
-
-                if ref_length >= len(byte_stream) or ref_length > MAX_REF_LENGTH:
-                    break
-
-                ref_length += 1
-
-            if ref_length >= 3:
-                break
-
-            ref_dist += 1
+        ref_length, ref_dist = find_reference(byte_stream, byte_idx)        
             
-        if ref_length < 3:
-            code = reverse_bits(tree_codes[byte],tree_lengths[byte]) 
-            out_stream.append(code, tree_lengths[byte])
-            byte_idx += 1
-        else:
-            label, num_bits, offset = LENGTH_CODES[ref_length]
-            code = reverse_bits(tree_codes[label],tree_lengths[label])
-            out_stream.append(code, tree_lengths[label])
-            out_stream.append(offset, num_bits)
+        if ref_length >= 3:
 
-            label, num_bits, offset = DISTANCE_CODES[ref_dist]
-            code = reverse_bits(distance_codes[label],distance_lengths[label])
-            out_stream.append(code, distance_lengths[label])
-            out_stream.append(offset, num_bits)
+            len_label, len_num_bits, len_offset = LENGTH_CODES[ref_length]
+            dist_label, dist_num_bits, dist_offset = DISTANCE_CODES[ref_dist]
 
-            byte_idx += ref_length
+            total_ref_size = tree_lengths[len_label] + len_num_bits + distance_lengths[dist_label] + dist_num_bits
+            if 8 * ref_length > total_ref_size:
 
+                out_stream.append_reverse(tree_codes[len_label], tree_lengths[len_label])
+                out_stream.append(len_offset, len_num_bits)
+
+                out_stream.append_reverse(distance_codes[dist_label], distance_lengths[dist_label])
+                out_stream.append(dist_offset, dist_num_bits)
+
+                byte_idx += ref_length
+                continue
+            
+ 
+        out_stream.append_reverse(tree_codes[byte], tree_lengths[byte])
+        byte_idx += 1
 
     return out_stream.get()
 
